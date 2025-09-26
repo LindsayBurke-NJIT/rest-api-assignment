832eab1c6c6d3cdee18087c959a9e025aee3fd0d
Lindsay Burke <lrb@njit.edu>
2025-09-25 22:22:35 -0400

    handled endpoints

--- src/index.js
+++ src/index.js
@@ -7,12 +7,56 @@ app.use(express.json());
 
 // **************************************************************
 // Put your implementation here
+const { v4: uuidv4 } = require('uuid');
+const userList = [];
+
+app.post('/users', (req, res) => {
+    const {name, email} = req.body;
+    if (!name || !email){
+        res.status(400).json({error: 'Name and email are required.'});
+    }
+    else{
+        const id = uuidv4();
+        const user = {id, name, email};
+        userList.push(user);
+        res.status(201).json(user);
+    }
+})
 // If necessary to add imports, please do so in the section above
 
-app.get('/', (req, res) => {
-    res.send('Hello World!');
+app.get('/users/:id', (req, res) => {
+    const userID = req.params.id;
+    const currUser = userList.find(u => u.id === userID)
+    if (!currUser){
+        res.status(404).json({error: ''});
+    }
+    else{
+        res.status(200).json(currUser);
+    }
 });
 
+app.put('/users/:id', (req, res) => {
+    const user = userList.find(u => u.id === req.params.id);
+    if (!user) {
+        return res.status(404).json({ error: 'Could not find specified user.' });
+    }
+    const { name, email } = req.body;
+    if (!name || !email) {
+        return res.status(400).json({ error: 'Name and email are required.' });
+    }
+    user.name = name;
+    user.email = email;
+    res.status(200).json(user);
+});
+
+app.delete('/users/:id', (req, res) => {
+    const index = userList.findIndex(u => u.id === req.params.id);
+    if (index === -1) {
+        return res.status(404).json({error: 'Could not find specified user.'});
+    }
+    userList.splice(index, 1);
+    res.status(204).send();
+});
 // Do not touch the code below this comment
 // **************************************************************
 
